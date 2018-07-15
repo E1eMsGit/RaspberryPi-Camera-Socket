e9b1d886d98d4d012095e80065c48157d741eb74
E1eM <simaginam@gmail.com>
2018-07-15 13:20:21 +0300

    v 1.3 add snapshot

--- client.py
+++ client.py
@@ -1,6 +1,4 @@
-import datetime
 import io
-import os
 import queue
 import socket
 import struct
@@ -13,97 +11,81 @@ import numpy as np
 from PIL import Image, ImageTk
 
 
-def init_ui():
-    bg_color = "#FFFFFF"
+class VideoStream(object):
 
-    root.title(app_name)
-    root.resizable(width=False, height=False)
-    root.configure(bg='#B0BEC5')
-    root.protocol("WM_DELETE_WINDOW", lambda: close_event())
+    def __init__(self, master):
+        self.master = master
 
-    video_frame = tk.Frame(root, bg=bg_color, bd=1, relief="raised", width=740,
-                           height=596)
-    control_frame = tk.Frame(root, bg=bg_color, bd=1, relief="raised",
-                             width=740, height=50)
+        self.master.title(app_name)
+        self.master.resizable(width=False, height=False)
+        self.master.configure(bg='#B0BEC5')
+        self.master.protocol("WM_DELETE_WINDOW", lambda: self.close_event())
 
-    video_label = tk.Label(video_frame, bd=2, relief="groove", height=576,
-                           width=720, font=("Source Code Pro", 16, "bold"))
-    snapshot_button = tk.Button(control_frame, command=take_snapshot,
-                                font=("Source Code Pro", 16, "bold"),
-                                text="Snapshot", height=50, width=150)
+        self.video_frame = tk.Frame(root, bg="#FFFFFF", bd=1, relief="raised",
+                                    width=740, height=596)
 
-    for frame in [video_frame, control_frame]:
-        frame.pack(padx=10, pady=10)
-        frame.pack_propagate(0)
+        self.video_label = tk.Label(self.video_frame, bd=2, relief="groove",
+                                    height=576, width=720,
+                                    font=("Source Code Pro", 16, "bold"))
 
-    video_label.pack(padx=10, pady=10)
-    snapshot_button.pack(padx=5, pady=5)
+        self.video_frame.pack(padx=10, pady=10)
+        self.video_frame.pack_propagate(0)
+        self.video_label.pack(padx=10, pady=10)
 
-    update_image(video_label)
+        self.update_image()
 
+    def update_image(self):
+        """
+        Обновление изображения видеофрейма.
+        :return:
+        """
+        global status_index
+        global queue_data
+        connection_status = {0: "Attempting to connect to the server...",
+                             1: "Connection complete",
+                             2: "Server is not running. Connection fail"}
 
-def update_image(video_label: tk.Label):
-    """
-    Обновление изображения видеофрейма.
-    :param video_label: Видеофрейм.
-    :return:
-    """
-    global status_index
-    global queue_data
-    connection_status = {0: "Attempting to connect to the server...",
-                         1: "Connection complete",
-                         2: "Server is not running. Connection fail"}
+        if not connection_status_q.empty():
+            status_index = connection_status_q.get()
+            self.video_label.configure(text=connection_status.get(status_index))
+            connection_status_q.task_done()
+        else:
+            if status_index in [0, 2]:
+                self.video_label.configure(
+                    text=connection_status.get(status_index))
+            elif status_index == 1:
+                queue_data = video_stream_q.get()
 
-    if not connection_status_q.empty():
-        status_index = connection_status_q.get()
-        video_label.configure(text=connection_status.get(status_index))
-        connection_status_q.task_done()
-    else:
-        if status_index in [0, 2]:
-            video_label.configure(text=connection_status.get(status_index))
-        elif status_index == 1:
-            queue_data = video_stream_q.get()
+                a = Image.fromarray(queue_data)
+                b = ImageTk.PhotoImage(image=a)
+                self.video_label.configure(image=b)
+                video_stream_q.task_done()
 
-            a = Image.fromarray(queue_data)
-            b = ImageTk.PhotoImage(image=a)
-            video_label.configure(image=b)
-            video_stream_q.task_done()
+        root.update()
+        root.after(0, func=lambda: self.update_image())
 
-    root.update()
-    root.after(0, func=lambda: update_image(video_label))
+    def close_event(self):
+        """
+        Уничтожает процессы.
+        Уничтожает пользовательский интерфейс.
+        Закрывает соединение.
+        :return:
+        """
+        global stop_thread
+        result = messagebox.askquestion(app_name,
+                                        "Are you sure you want to exit?",
+                                        icon='question')
 
-
-def take_snapshot():
-    global queue_data
-
-    ts = datetime.datetime.now()
-    filename = "{}.jpg".format(ts.strftime("%Y-%m-%d_%H-%M-%S"))
-    path = os.path.sep.join(("photo", filename))
-
-    cv2.imwrite(path, cv2.cvtColor(queue_data, cv2.COLOR_BGR2RGB))
-
-
-def close_event():
-    """
-    Уничтожает процессы.
-    Уничтожает пользовательский интерфейс.
-    Закрывает соединение.
-    :return:
-    """
-    global stop_thread
-    result = messagebox.askquestion(app_name, "Are you sure you want to exit?",
-                                    icon='question')
-
-    if result == 'yes':
-        if connection_t.is_alive():
-            connection_t.join()
-        if video_stream_t.is_alive():
-            stop_thread = True
-            video_stream_t.join()
-        root.destroy()
-        client_socket.close()
-    else:
-        pass
+        if result == 'yes':
+            if connection_t.is_alive():
+                connection_t.join()
+            if video_stream_t.is_alive():
+                stop_thread = True
+                video_stream_t.join()
+            root.destroy()
+            client_socket.close()
+        else:
+            pass
 
 
 def open_connection():
@@ -158,14 +140,13 @@ if __name__ == "__main__":
     stop_thread = False
     queue_data = None
 
-    video_stream_q = queue.Queue()
-    connection_status_q = queue.Queue()
-
-    video_stream_t = threading.Thread(target=get_stream_loop)
     connection_t = threading.Thread(target=open_connection)
+    connection_status_q = queue.Queue()
+    video_stream_t = threading.Thread(target=get_stream_loop)
+    video_stream_q = queue.Queue()
 
     connection_t.start()
 
     root = tk.Tk()
-    init_ui()
+    app = VideoStream(root)
     root.mainloop()
