5853a358344d0bad817c3ea79f4c374e66068bbb
E1eM <simaginam@gmail.com>
2018-07-15 07:31:30 +0300

    v 1.3 add snapshot

--- client.py
+++ client.py
@@ -1,4 +1,6 @@
+import datetime
 import io
+import os
 import queue
 import socket
 import struct
@@ -10,13 +12,6 @@ import cv2
 import numpy as np
 from PIL import Image, ImageTk
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.settimeout(5)
-
-server_ip = "192.168.1.4"
-server_port = 9090
-app_name = "RaspberryPi Video Streaming"
-
 
 def init_ui():
     bg_color = "#FFFFFF"
@@ -24,7 +19,7 @@ def init_ui():
     root.title(app_name)
     root.resizable(width=False, height=False)
     root.configure(bg='#B0BEC5')
-    root.protocol("WM_DELETE_WINDOW", lambda: close_connection())
+    root.protocol("WM_DELETE_WINDOW", lambda: close_event())
 
     video_frame = tk.Frame(root, bg=bg_color, bd=1, relief="raised", width=740,
                            height=596)
@@ -33,12 +28,16 @@ def init_ui():
 
     video_label = tk.Label(video_frame, bd=2, relief="groove", height=576,
                            width=720, font=("Source Code Pro", 16, "bold"))
+    snapshot_button = tk.Button(control_frame, command=take_snapshot,
+                                font=("Source Code Pro", 16, "bold"),
+                                text="Snapshot", height=50, width=150)
 
     for frame in [video_frame, control_frame]:
         frame.pack(padx=10, pady=10)
         frame.pack_propagate(0)
 
     video_label.pack(padx=10, pady=10)
+    snapshot_button.pack(padx=5, pady=5)
 
     update_image(video_label)
 
@@ -50,6 +49,7 @@ def update_image(video_label: tk.Label):
     :return:
     """
     global status_index
+    global queue_data
     connection_status = {0: "Attempting to connect to the server...",
                          1: "Connection complete",
                          2: "Server is not running. Connection fail"}
@@ -59,11 +59,12 @@ def update_image(video_label: tk.Label):
         video_label.configure(text=connection_status.get(status_index))
         connection_status_q.task_done()
     else:
-        if status_index == 0 or status_index == 2:
+        if status_index in [0, 2]:
             video_label.configure(text=connection_status.get(status_index))
         elif status_index == 1:
-            video_frame = video_stream_q.get()
-            a = Image.fromarray(video_frame)
+            queue_data = video_stream_q.get()
+
+            a = Image.fromarray(queue_data)
             b = ImageTk.PhotoImage(image=a)
             video_label.configure(image=b)
             video_stream_q.task_done()
@@ -72,46 +73,17 @@ def update_image(video_label: tk.Label):
     root.after(0, func=lambda: update_image(video_label))
 
 
-def video_loop():
-    global stop_thread
-    global connection
+def take_snapshot():
+    global queue_data
 
-    while stop_thread is False:
-        img_len = struct.unpack("<L", connection.read(
-            struct.calcsize("<L")))[0]
-        if not img_len:
-            break
+    ts = datetime.datetime.now()
+    filename = "{}.jpg".format(ts.strftime("%Y-%m-%d_%H-%M-%S"))
+    path = os.path.sep.join(("photo", filename))
 
-        image_stream = io.BytesIO()
-        image_stream.write(connection.read(img_len))
-        image_stream.seek(0)
-
-        data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
-        image = cv2.imdecode(data, 1)
-        rgba_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
-
-        video_stream_q.put(rgba_image)
+    cv2.imwrite(path, cv2.cvtColor(queue_data, cv2.COLOR_BGR2RGB))
 
 
-def open_connection():
-    """
-    Устанавливает соединение с сервером.
-    :return:
-    """
-    global connection
-
-    try:
-        client_socket.connect((server_ip, server_port))
-        connection_status = 1
-        connection = client_socket.makefile("rb")
-        video_stream_t.start()
-    except socket.timeout:
-        connection_status = 2
-    finally:
-        connection_status_q.put(connection_status)
-
-
-def close_connection():
+def close_event():
     """
     Уничтожает процессы.
     Уничтожает пользовательский интерфейс.
@@ -134,15 +106,62 @@ def close_connection():
         pass
 
 
+def open_connection():
+    """
+    Устанавливает соединение с сервером.
+    :return:
+    """
+    global connection
+
+    try:
+        client_socket.connect((server_ip, server_port))
+        connection_status = 1
+        connection = client_socket.makefile("rb")
+        video_stream_t.start()
+    except socket.timeout:
+        connection_status = 2
+    finally:
+        connection_status_q.put(connection_status)
+
+
+def get_stream_loop():
+    global stop_thread
+    global connection
+
+    while stop_thread is False:
+        img_len = struct.unpack("<L", connection.read(
+            struct.calcsize("<L")))[0]
+        if not img_len:
+            break
+
+        image_stream = io.BytesIO()
+        image_stream.write(connection.read(img_len))
+        image_stream.seek(0)
+
+        data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
+        image = cv2.imdecode(data, 1)
+        rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        video_stream_q.put(rgb_image)
+
+
 if __name__ == "__main__":
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.settimeout(5)
+
+    server_ip = "192.168.1.4"
+    server_port = 9090
+    app_name = "RaspberryPi Video Streaming"
+
     connection = None
     status_index = 0
     stop_thread = False
+    queue_data = None
 
     video_stream_q = queue.Queue()
     connection_status_q = queue.Queue()
 
-    video_stream_t = threading.Thread(target=video_loop)
+    video_stream_t = threading.Thread(target=get_stream_loop)
     connection_t = threading.Thread(target=open_connection)
 
     connection_t.start()
